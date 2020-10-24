ed863539d8d0733168eef9bc48ec07d9a16a24cb
simondyc <44817289+simondyc@users.noreply.github.com>
2020-10-24 17:16:44 -0500

    tableroR
    Agregue algo

---
+++ app.R
@@ -0,0 +1,15 @@
+## app.R ##
+library(shiny)
+library(shinydashboard)
+
+ui <- dashboardPage(
+  dashboardHeader(),
+  dashboardSidebar(),
+  dashboardBody()
+)
+
+server <- function(input, output) {
+  "Aquí van un comentario"
+}
+
+shinyApp(ui, server)
